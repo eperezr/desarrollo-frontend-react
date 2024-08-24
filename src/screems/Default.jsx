79f71669233aabf6cdd587c8f88e38a70faee235
Eddy Perez <eddyperez007@hotmail.com>
2024-08-24 00:16:06 -0400

    four commit

--- src/screems/Default.jsx
+++ src/screems/Default.jsx
@@ -1,11 +1,13 @@
 import Logo from './_components/Logo.jsx';
 import Description from "./_components/Description.jsx";
 import OpenLink from '../components/OpenLink.jsx';
+import Contador from './_components/Contador.jsx';
 
 const Default = () => {
     return (
         <>
             <header className="App-header">
+                <Contador />
                 <Logo />
                 <Description />
                 <OpenLink
