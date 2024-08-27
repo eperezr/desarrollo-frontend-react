da9306ef0177b6edc06e841faa6f2fa582075d3e
Eddy Perez <eddyperez007@hotmail.com>
2024-08-26 23:37:39 -0400

    monday

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+    const { username, email } = useSelector(state => state.form.formData);
     return (
         <nav>
             <ul>
@@ -13,10 +15,18 @@ const Navbar = () => {
                 <li>
                     <Link to="/product">Product</Link>
                 </li>
-                <li>
-                    <Link to="/login">Login Form</Link>
-                </li>
             </ul>
+            <ul>
+                {username ? (
+             <li>
+                <Link to="/login">Usuario: {username} Email: {email}</Link>
+            </li>): 
+            (<li>
+                <Link to="/login">Login Form</Link>
+            </li>
+            )}
+      </ul>
+
         </nav>
     );
 };
